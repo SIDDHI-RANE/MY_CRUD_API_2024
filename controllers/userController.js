2f2ca6988cce4af5fb786cc4c011c8d6274e43b1
SIDDHIRANE-ALT-PI <siddhirane.altpi@gmail.com>
2024-08-21 16:32:56 +0530

    MY first CRUD project API with MONGO DB

---
+++ controllers/userController.js
@@ -0,0 +1,82 @@
+import User from '../models/userModel.js';
+
+// @desc Get all users
+// @route GET /api/users
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// @desc Create a user
+// @route POST /api/users
+export const createUser = async (req, res) => {
+  const { name, email, age } = req.body;
+
+  const user = new User({ name, email, age });
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// @desc Get user by ID
+// @route GET /api/users/:id
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// @desc Update a user
+// @route PUT /api/users/:id
+export const updateUser = async (req, res) => {
+  const { name, email, age } = req.body;
+
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (user) {
+      user.name = name || user.name;
+      user.email = email || user.email;
+      user.age = age || user.age;
+
+      const updatedUser = await user.save();
+      res.json(updatedUser);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// @desc Delete a user
+// @route DELETE /api/users/:id
+export const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (user) {
+      await user.deleteOne();
+      res.json({ message: 'User removed' });
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
